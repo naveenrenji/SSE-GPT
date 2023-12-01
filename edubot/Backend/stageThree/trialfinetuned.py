6184b279403f7c77de25ee7228183e7e316669d1
Naveen Mathews <naveenrenji55@gmail.com>
2023-11-30 21:14:26 -0500

    LLama

---
+++ edubot/Backend/stageThree/trialfinetuned.py
@@ -0,0 +1,40 @@
+from transformers import AutoTokenizer,AutoModelForCausalLM
+from peft import PeftModel, PeftConfig
+
+def load_model():
+    model_id = "kings-crown/EM624_QA_Full"
+    base_model_id = "meta-llama/Llama-2-13b-chat-hf"
+    access_token = "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"
+
+    # Load the base model
+    base_model = AutoModelForCausalLM.from_pretrained(base_model_id, use_auth_token=access_token)
+    tokenizer = AutoTokenizer.from_pretrained(base_model_id, use_auth_token=access_token)
+    
+    # Load the PEFT-configured model
+    config = PeftConfig.from_pretrained(model_id, use_auth_token=access_token)
+    model = PeftModel.from_pretrained(base_model, config=config, model_id=model_id, use_auth_token=access_token)
+
+    return model, tokenizer
+
+
+def generate_response(model, tokenizer, query):
+    # Encode and generate a response
+    inputs = tokenizer.encode(query, return_tensors='pt')
+    output = model.generate(inputs, max_length=512, num_return_sequences=1, temperature=1.0)
+    response_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    return response_text
+
+def main():
+    model, tokenizer = load_model()
+    print("Model loaded. You can start chatting. Type 'quit' to exit.")
+
+    while True:
+        query = input("You: ")
+        if query.lower() == 'quit':
+            break
+
+        response = generate_response(model, tokenizer, query)
+        print("Bot:", response)
+
+if __name__ == "__main__":
+    main()
