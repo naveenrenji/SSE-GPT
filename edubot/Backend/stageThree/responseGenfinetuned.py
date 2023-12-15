d2134938911de7fabd9019074393779b1033c127
Naveen Mathews <naveenrenji55@gmail.com>
2023-12-15 15:19:06 -0500

    fix

--- edubot/Backend/stageThree/responseGenfinetuned.py
+++ edubot/Backend/stageThree/responseGenfinetuned.py
@@ -2,30 +2,30 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from peft import PeftModel, PeftConfig
 from torch import cuda, bfloat16
 
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,                         # load the model in 4-bit precision.
+    bnb_4bit_quant_type='nf4',                 # type of quantization to use for 4-bit weights.
+    bnb_4bit_use_double_quant=True,            # use double quantization for 4-bit weights.
+    bnb_4bit_compute_dtype=bfloat16            # compute dtype to use for 4-bit weights.
+)
+
 def load_model():
     model_id = 'kings-crown/EM624_QA_Multi'
     base_model_id = "meta-llama/Llama-2-13b-chat-hf"
     access_token = "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"
-
-    # Use BitsAndBytesConfig for efficient model loading
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,                         # load the model in 4-bit precision.
-        bnb_4bit_quant_type='nf4',                 # type of quantization to use for 4-bit weights.
-        bnb_4bit_use_double_quant=True,            # use double quantization for 4-bit weights.
-        bnb_4bit_compute_dtype=bfloat16            # compute dtype to use for 4-bit weights.
-    )
-
-    # Load the base model with the custom configuration
-    base_model = AutoModelForCausalLM.from_pretrained(base_model_id, use_auth_token=access_token, quantization_config=bnb_config)
-    
-    # Load the tokenizer
+    base_model = AutoModelForCausalLM.from_pretrained(base_model_id, use_auth_token=access_token)
     tokenizer = AutoTokenizer.from_pretrained(base_model_id, use_auth_token=access_token)
-    
-    # Load the PEFT-configured model
     config = PeftConfig.from_pretrained(model_id, use_auth_token=access_token)
-    model = PeftModel.from_pretrained(base_model, config=config, model_id=model_id, use_auth_token=access_token)
-
+    model = AutoModelForCausalLM.from_pretrained(
+        config.base_model_name_or_path,
+        return_dict=True,
+        quantization_config=bnb_config,
+        device_map="auto",
+        trust_remote_code=True
+    )
     return model, tokenizer
+
 
 conversation_history = []
 
@@ -47,7 +47,8 @@ def response_generator(model, tokenizer, question, context):
 
     # Generate a response using the model
     inputs = tokenizer.encode(formatted_input, return_tensors="pt")
-    output = model.generate(inputs, max_length=512, num_return_sequences=1, temperature=1.0)
+    inputs = inputs.to(model.device)
+    output = model.generate(inputs, max_length=512, num_return_sequences=1, temperature=0.1)
     response_text = tokenizer.decode(output[0], skip_special_tokens=True)
 
     # Append the model's response to the conversation history
